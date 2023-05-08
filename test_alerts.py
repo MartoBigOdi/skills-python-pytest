831ff4c0b6f59bf7bbdd466994ee2522129bb9c5
Marto Vasconcelo <martin.vasconcelo@gmail.com>
2023-05-08 14:59:38 -0300

    sumamos mas webelements para test

---
+++ test_alerts.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from utility import tools
+import pytest
+import time
+
+#setting driver
+chr_options = Options()
+chr_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chr_options)
+driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+driver.maximize_window()
+
+try:
+
+    buttonAlert = driver.find_element(By.XPATH, "//button[contains(text(),'Click for JS Alert')]")
+    tools.highlight(buttonAlert)
+    buttonAlert.click()
+    time.sleep(2)
+    # aceptamos el alert importando la clase Alert
+    Alert(driver).accept()
+    # otra forma es directamente desde driver e ir ingresando a sus clases para buscar el metodo accept()
+    # driver.switch_to.alert.accept()
+    # tambien podemos hascer dismiss() al alert
+    # Alert(driver).dismiss()
+    time.sleep(2)
+
+
+    @pytest.mark.alert
+    def test_buttonAlert():
+        assert buttonAlert is not None
+
+finally:
+  driver.quit()
