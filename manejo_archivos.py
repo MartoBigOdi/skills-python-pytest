a47b081e991c1997312ffdac21b840e493371433
Marto Vasconcelo <martin.vasconcelo@gmail.com>
2023-05-25 11:10:06 -0300

    actualizacion manejo de archivos

---
+++ manejo_archivos.py
@@ -0,0 +1,20 @@
+# Lectura de archivo
+# f = open("archivo.txt", "r")
+# # print(f.readlines())
+#
+# #imprimimos el contenido del archivo sin el caracter del final que veiamos con el print anterior
+# for line in f:
+#     line = line.strip()
+#     line = line.split(",")
+#     print(line[1])
+
+
+# Escritura del arhivo
+# a = open("archivo.txt", "a")
+# a.write("Limon, 1, 8, 4\n")
+
+
+# w =open("archivo2.txt", "w")
+# w.write("Hola Mundo")
+
+
